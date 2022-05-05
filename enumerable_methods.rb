3e677a7fc85aeabd52f9d43adc0be36cb5a82f39
yersel500 <yersel500@gmail.com>
2022-05-05 17:16:09 -0500

    add filter method and solve linter errors

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -3,9 +3,16 @@ module MyEnumerable
     each { |item| return false unless yield(item) }
     true
   end
+
   def any?
     each { |item| return true unless yield(item) }
     false
+  end
+
+  def filter
+    new_list = []
+    each { |item| new_list << item if yield(item) }
+    new_list
   end
 end
 
@@ -22,6 +29,8 @@ end
 
 array = MyList.new([1, 2, 3, 4])
 
-puts array.all? { |num| num <= 5 }
+p(array.all? { |num| num <= 5 })
 
-list_item.any? { |num| num == 2 }
+p(array.any? { |num| num == 2 })
+
+p array.filter(&:even?)
