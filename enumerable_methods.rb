f79485e0c8da00a50c03d53e213484a51b8d305f
yersel500 <yersel500@gmail.com>
2022-05-05 16:51:16 -0500

    finish all method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -2,6 +2,7 @@ module MyEnumerable
   def all?
     each { |item| return false unless yield(item) }
     true
+  end
 end
 
 class MyList
@@ -12,14 +13,9 @@ class MyList
 
   def each(&block)
     @list.each(&block)
-    end
   end
 end
 
-# list = MyList.new([1, 2, 3, 4])
+array = MyList.new([1, 2, 3, 4])
 
-# puts list.any? { |num| num >= 3 }
-
-# puts list.all? { |num| num <= 3 }
-
-# p list.filter { |num| num.even? }
+puts array.all? { |num| num <= 5 }
