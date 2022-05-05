1e6ba4b3e5085e4e49dcbda1844e01107155bc40
Estebanmual <esteban.munozal@gmail.com>
2022-05-05 17:04:25 -0500

    Added any method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -3,6 +3,10 @@ module MyEnumerable
     each { |item| return false unless yield(item) }
     true
   end
+  def any?
+    each { |item| return true unless yield(item) }
+    false
+  end
 end
 
 class MyList
@@ -19,3 +23,5 @@ end
 array = MyList.new([1, 2, 3, 4])
 
 puts array.all? { |num| num <= 5 }
+
+list_item.any? { |num| num == 2 }
